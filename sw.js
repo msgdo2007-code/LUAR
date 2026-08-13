20eddfa79f01d72cc7a49b406a357c146f3287d2
msgdo2007-code <msgdo2007-code@users.noreply.github.com>
2026-08-13 18:45:21 -0300

    force fresh production assets

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const LUAR_SHELL_CACHE = 'luar-shell-v2';
+const LUAR_SHELL_CACHE = 'luar-shell-v3';
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(LUAR_SHELL_CACHE).then(cache => cache.addAll(['/manifest.webmanifest', '/scripts/icon-192.png', '/scripts/icon-512.png'])).catch(() => null));
   self.skipWaiting();
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('luar-shell-') && key !== LUAR_SHELL_CACHE).map(key => caches.delete(key)))));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== LUAR_SHELL_CACHE).map(key => caches.delete(key)))));
   self.clients.claim();
 });
